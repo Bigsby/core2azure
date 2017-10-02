b6f27727b4326485cb6b7f09a61260899e5ede27
Bigsby <bigsby@live.com>
2017-10-02 21:36:19 +0100

    More updates

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -5,4 +5,7 @@ public class TestController : ControllerBase
 {
     [HttpGet("")]
     public IActionResult Get() => Ok("This is an updated controller");
+
+    [HttpGet("{value}")]
+    public IActionResult GetValue(string value) => Ok($"You sent {value}");
 }
